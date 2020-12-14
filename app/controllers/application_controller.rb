2f832409897e563fca5ade12d7fab4be37eeb239
GitHubのユーザ名 <GitHub>
2020-12-14 13:25:57 +0000

    [Fix]4-10

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,11 +4,11 @@ class ApplicationController < ActionController::Base
     def after_sign_in_path_for(resource)
       user_path(current_user) # ログイン後に遷移するpathを設定
     end
-  
+
     def after_sign_out_path_for(resource)
       root_path # ログアウト後に遷移するpathを設定
     end
-    
+
   protected
 
 
--- db/schema.rb
+++ db/schema.rb
@@ -44,7 +44,7 @@ ActiveRecord::Schema.define(version: 2020_12_13_120530) do
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
     t.string "name"
-    t.text "introduce"
+    t.text "introduction"
     t.string "profile_image_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
