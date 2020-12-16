ccb62e07311d22dae062414621bfd9ea1d984bab
GitHubのユーザ名 <GitHub>
2020-12-16 05:18:42 +0000

    [Fix4-10

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = 'd97e958b6fe2d929ca411e111d77550125be6fa1d5f2d4f9488cf628433b499717527caaad68733927d1628ac54b6d95eb1390b2d8f591dc26046ad4f8362583'
