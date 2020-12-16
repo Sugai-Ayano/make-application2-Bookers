86e4db14587fc5e97c5eb2ddb5f99585052bcdd8
GitHubのユーザ名 <GitHub>
2020-12-16 06:50:39 +0000

    [Fix]4-10

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,5 @@ root to: 'homes#top'
  resources :books, only: [:new, :create, :index, :show, :destroy, :top]
  resources :book_comments, only: [:create, :destroy]
  resources :users, only: [:show, :index, :edit, :update]
+ post 'books' => 'books#create'
 end
