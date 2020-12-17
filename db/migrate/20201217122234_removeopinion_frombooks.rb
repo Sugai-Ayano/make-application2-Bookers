a15eba939979f8affff63b28d56e57d4e836c11e
GitHubのユーザ名 <GitHub>
2020-12-17 12:44:51 +0000

    [Fix]4-10

---
+++ db/migrate/20201217122234_removeopinion_frombooks.rb
@@ -0,0 +1,5 @@
+class RemoveopinionFrombooks < ActiveRecord::Migration[5.2]
+ def change
+    remove_column :books, :opinion, :text
+ end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_16_074331) do
+ActiveRecord::Schema.define(version: 2020_12_17_122234) do
 
   create_table "books", force: :cascade do |t|
     t.text "profile_image_id"
@@ -19,8 +19,8 @@ ActiveRecord::Schema.define(version: 2020_12_16_074331) do
     t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.text "opinion"
     t.string "title"
+    t.text "body"
   end
 
   create_table "users", force: :cascade do |t|
